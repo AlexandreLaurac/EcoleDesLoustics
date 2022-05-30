80294dbdff573b90d4572026debffa9bd14111b2
baldojerome <99471537+baldojerome@users.noreply.github.com>
2022-05-30 23:57:11 +0200

    Create HighTechItem.java

---
+++ app/src/main/java/com/example/ecoleloustics/Model/HighTechItem.java
@@ -0,0 +1,28 @@
+package com.example.ecoleloustics.Model;
+
+public class HighTechItem {
+
+    //attributs
+
+    private String name;
+    private String prenom;
+
+    //constructeurs
+
+
+    public HighTechItem(String name, String prenom) {
+        this.name = name;
+        this.prenom = prenom;
+    }
+
+    //méthodes
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+}
