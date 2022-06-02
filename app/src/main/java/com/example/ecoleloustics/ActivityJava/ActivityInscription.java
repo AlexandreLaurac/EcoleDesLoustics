925d02e55557336d4c2227afb7f65c9b252c0218
baldoj <baldo.jerome@gmail.com>
2022-06-02 11:24:41 +0200

    mise à jour jeje

--- app/src/main/java/com/example/ecoleloustics/ActivityJava/ActivityInscription.java
+++ app/src/main/java/com/example/ecoleloustics/ActivityJava/ActivityInscription.java
@@ -17,13 +17,25 @@ public class ActivityInscription extends AppCompatActivity {
         // On charge le XML pour créer l'arbre graphique
         setContentView(R.layout.activity_inscription);
     }
+// methode vérifiant que les champs sont remplis
+
+
+
+//methode  activant ou désactivant le bouton valider
+
+    // si champsOk retourne true
+    // dans ce cas valider passe en true et change l'état du bouton
+    // si champOK retourne False
+    // dans ce cas valider continue de rester en false
+    // si alors toast affichant que les champs ne sont pas tous remplis
+
 
     public void onMenu(View view) {
         //verifier si champs remplis
         // si pas champ rempli toast pour signaler l'erreur
         // Création d'une intention
         Intent activityMenu = new Intent(ActivityInscription.this , ActivityMenu.class);
-
+        // appeler méthode
         // Lancement de la demande de changement d'activité
         startActivity(activityMenu);
     }
---
+++ app/src/main/java/com/example/ecoleloustics/ActivityJava/Addition.java
@@ -0,0 +1,50 @@
+package com.example.ecoleloustics.ActivityJava;
+
+public class Addition {
+    // Attributs
+    private int operande1 ;
+    private int operande2 ;
+
+    // Constructeurs
+    public Addition() {
+        this.operande1 = 1 ;
+        this.operande2 = 1 ;
+    }
+
+    public Addition (int operande1, int operande2) {
+        this.operande1 = operande1 ;
+        this.operande2 = operande2 ;
+    }
+
+    // Getters
+    public int getOperande1 () {
+        return operande1 ;
+    }
+
+    public int getOperande2 () {
+        return operande2 ;
+    }
+
+    public int getSomme () {
+        return operande1 + operande2 ;
+    }
+
+    // Setters
+    public void setOperande1 (int operande1) {
+        if (operande1 >= 1 && operande1 <= 9) {
+            this.operande1 = operande1 ;
+        }
+        else {
+            this.operande1 = 1 ;
+        }
+    }
+
+    public void setOperande2 (int operande2) {
+        if (operande2 >= 1 && operande2 <= 9) {
+            this.operande2 = operande2 ;
+        }
+        else {
+            this.operande2 = 1 ;
+        }
+    }
+}
